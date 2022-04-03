0d0dc2d0c123ae5d4ecdfae2df4e39c9aabac554
Your Name <tariqfaqira@gmail.com>
2022-04-03 13:31:30 +0300

    _custom.css is updated, footer and header is updated

--- src/layouts/components/Footer.jsx
+++ src/layouts/components/Footer.jsx
@@ -1,9 +1,73 @@
 import React from 'react'
+import { Grid, Box, Typography, Card, CardContent, TextField, Stack, Button } from '@mui/material';
 
 const Footer = () => {
   return (
-    <div style={{ height: '100px'}} >Footer</div>
+    <Box sx={{ backgroundColor: 'black'}}>
+      <Grid container sx={{ padding: "80px 0px 100px 99px" }} >
+        <Grid item xs={8}>
+          <Grid container spacing={10} flexDirection="column" >
+            <Grid item xs={4}>
+              <img
+                src={require('../../assets/images/logo.png')}
+                alt="logo"
+                className="footer-logo"
+              />
+              <Typography sx={{ color: "white" }} > Excepteur sint occaecat cupidatat non proident, sunt in culpa officia deserunt mollitid est laborum. Sed ut perspiciatis unde qui omnis iste natus error sit voluptatem. </Typography>
+
+            </Grid>
+            <Grid item xs={4}>
+              <Typography sx={{ color: "white" }}> A : No 40 Baria Sreet 133/2 NewYork City, </Typography>
+              <Typography sx={{ color: "white" }}> NY, United States </Typography>
+              <Typography sx={{ color: "white" }}>E: info.contact@gmail.com </Typography>
+              <Typography sx={{ color: "white" }}>  P: (00) 123 456 789 </Typography>
+            </Grid>
+          </Grid>
+        </Grid>
+        <Grid item xs={4}>
+          <Card style={{ marginRight: 100 }} >
+            <CardContent>
+              <Stack flexDirection='column' spacing={5} >
+                <Typography >We will love to hear from you!</Typography>
+                <TextField
+                  id="filled-multiline-flexible"
+                  label="Name"
+                  multiline
+                  maxRows={2}
+                  variant="filled"
+                />
+                <TextField
+                  id="filled-multiline-flexible"
+                  label="Email"
+                  multiline
+                  maxRows={2}
+                  variant="filled"
+                />
+                <TextField
+                  id="filled-multiline-flexible"
+                  label="Your Comment"
+                  multiline
+                  maxRows={4}
+                  variant="filled"
+                />
+                <Button sx={{ borderColor: "black", color: "black" }} variant="outlined">Send Message</Button>
+              </Stack>
+            </CardContent>
+          </Card>
+        </Grid>
+      </Grid>
+
+      <Grid container sx={{ padding: "80px 0px 100px 99px" }} >
+        <Grid item xs={8}>
+          <Typography sx={{ color: "white" }}>About | Customer Service | Terms & Conditions | Privacy Policy | Site Map | Contact</Typography>
+        </Grid>
+        <Grid item xs={4}>
+          <Typography style={{ color: "white", fontWeight: 'bold' }}>Copyright #LimeLife © 2022. All rights reserved.</Typography>
+        </Grid>
+      </Grid>
+    </Box>
   )
 }
 
 export default Footer
+
--- src/layouts/components/Header.jsx
+++ src/layouts/components/Header.jsx
@@ -1,99 +1,160 @@
 import * as React from 'react';
-import { styled, alpha } from '@mui/material/styles';
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 import IconButton from '@mui/material/IconButton';
 import Typography from '@mui/material/Typography';
-import InputBase from '@mui/material/InputBase';
+import Menu from '@mui/material/Menu';
 import MenuIcon from '@mui/icons-material/Menu';
+import Container from '@mui/material/Container';
+import Button from '@mui/material/Button';
+import MenuItem from '@mui/material/MenuItem';
+import { styled, alpha } from '@mui/material/styles';
 import SearchIcon from '@mui/icons-material/Search';
+import InputBase from '@mui/material/InputBase';
+import MailIcon from '@mui/icons-material/Mail';
+import { useNavigate } from 'react-router-dom';
 
-const Search = styled('div')(({ theme }) => ({
-  position: 'relative',
-  borderRadius: theme.shape.borderRadius,
-  backgroundColor: alpha(theme.palette.common.white, 0.15),
-  '&:hover': {
-    backgroundColor: alpha(theme.palette.common.white, 0.25),
-  },
-  marginLeft: 0,
-  width: '100%',
-  [theme.breakpoints.up('sm')]: {
-    marginLeft: theme.spacing(1),
-    width: 'auto',
-  },
-}));
-
-const SearchIconWrapper = styled('div')(({ theme }) => ({
-  padding: theme.spacing(0, 2),
-  height: '100%',
-  position: 'absolute',
-  pointerEvents: 'none',
-  display: 'flex',
-  alignItems: 'center',
-  justifyContent: 'center',
-}));
-
-const StyledInputBase = styled(InputBase)(({ theme }) => ({
-  color: 'inherit',
-  '& .MuiInputBase-input': {
-    padding: theme.spacing(1, 1, 1, 0),
-    // vertical padding + font size from searchIcon
-    paddingLeft: `calc(1em + ${theme.spacing(4)})`,
-    transition: theme.transitions.create('width'),
-    width: '100%',
-    [theme.breakpoints.up('sm')]: {
-      width: '12ch',
-      '&:focus': {
-        width: '20ch',
-      },
-    },
-  },
-}));
-
-const pages = ['HOME', 'PROTFOLIO', 'SERVICES', 'ABOUT US', 'CONTACT US'];
-
+const pages = ['HOME', 'PORTFOLIO', 'SERVICE', 'ABOUT US', 'CONTACT US'];
 
 const Header = () => {
-  return (
-    <Box sx={{ flexGrow: 1 }}>
-      <AppBar position="static">
-        <Toolbar>
-          <IconButton
-            size="large"
-            edge="start"
-            color="inherit"
-            aria-label="open drawer"
-            sx={{ mr: 2 }}
-          >
-            {/* <MenuIcon /> */}
-            <img
-              src={require("../../assets/images/logo.png")}
-              alt="logo"
-              className="header-logo"
-            />
-          </IconButton>
-          <Typography
-            variant="h6"
-            noWrap
-            component="div"
-            sx={{ flexGrow: 1, display: { xs: 'none', sm: 'block' } }}
-          >
-            MUI
-          </Typography>
-          <Search>
-            <SearchIconWrapper>
-              <SearchIcon />
-            </SearchIconWrapper>
-            <StyledInputBase
-              placeholder="Search…"
-              inputProps={{ 'aria-label': 'search' }}
-            />
-          </Search>
-        </Toolbar>
-      </AppBar>
-    </Box>
-  );
-}
+  const [anchorElNav, setAnchorElNav] = React.useState(null);
+  const navigate = useNavigate();
 
+  const handleOpenNavMenu = (event) => {
+    setAnchorElNav(event.currentTarget);
+  };
+
+  const handleCloseNavMenu = () => {
+    setAnchorElNav(null);
+  };
+
+  const handleNavigation = (value) => {
+    console.log("value", value);
+    // navigate('/dashboard', { replace: true });
+  }
+
+  const Search = styled('div')(({ theme }) => ({
+    position: 'relative',
+    borderRadius: theme.shape.borderRadius,
+    backgroundColor: alpha(theme.palette.common.white, 0.15),
+    '&:hover': {
+      backgroundColor: alpha(theme.palette.common.white, 0.25),
+    },
+    marginLeft: 0,
+    width: '100%',
+    [theme.breakpoints.up('sm')]: {
+      marginLeft: theme.spacing(1),
+      width: 'auto',
+    },
+  }));
+
+  const SearchIconWrapper = styled('div')(({ theme }) => ({
+    padding: theme.spacing(0, 2),
+    height: '100%',
+    position: 'absolute',
+    pointerEvents: 'none',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+  }));
+
+  const StyledInputBase = styled(InputBase)(({ theme }) => ({
+    color: 'inherit',
+    '& .MuiInputBase-input': {
+      padding: theme.spacing(1, 1, 1, 0),
+      // vertical padding + font size from searchIcon
+      paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+      transition: theme.transitions.create('width'),
+      width: '100%',
+      [theme.breakpoints.up('sm')]: {
+        width: '12ch',
+        '&:focus': {
+          width: '20ch',
+        },
+      },
+    },
+  }));
+
+  return (
+    // <AppBar position="static" sx={{ backgroundColor: 'red' }} >
+      <Container maxWidth="xl">
+        <Toolbar disableGutters>
+          <img
+            src={require('../../assets/images/logo.png')}
+            alt="logo"
+            className="header-logo"
+          />
+          <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
+            <IconButton
+              size="large"
+              aria-label="account of current user"
+              aria-controls="menu-appbar"
+              aria-haspopup="true"
+              onClick={handleOpenNavMenu}
+              color="inherit"
+            >
+              <MenuIcon />
+            </IconButton>
+            <Menu
+              id="menu-appbar"
+              anchorEl={anchorElNav}
+              anchorOrigin={{
+                vertical: 'bottom',
+                horizontal: 'left',
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: 'top',
+                horizontal: 'left',
+              }}
+              open={Boolean(anchorElNav)}
+              onClose={handleCloseNavMenu}
+              sx={{
+                display: { xs: 'block', md: 'none' },
+              }}
+            >
+              {pages.map((page) => (
+                <MenuItem key={page} onClick={handleCloseNavMenu}>
+                  <Typography textAlign="center">{page}</Typography>
+                </MenuItem>
+              ))}
+            </Menu>
+          </Box>
+
+          <Box sx={{ flexGrow: 1, justifyContent: 'center', display: { xs: 'none', md: 'flex' } }}>
+            {pages.map((page) => (
+              <Button
+                key={page}
+                onClick={handleCloseNavMenu}
+                sx={{ my: 2, color: 'black', display: 'block' }}
+              >
+                {page}
+              </Button>
+            ))}
+          </Box>
+
+          <Box sx={{ flexGrow: 0 }}>
+            <Search>
+              <SearchIconWrapper>
+                <SearchIcon />
+              </SearchIconWrapper>
+              <StyledInputBase
+                placeholder="Search…"
+                inputProps={{ 'aria-label': 'search' }}
+              />
+            </Search>
+
+          </Box>
+          <Box sx={{ flexGrow: 0 }}>
+            <IconButton size="large" aria-label="show 4 new mails" color="inherit">
+              <MailIcon />
+            </IconButton>
+          </Box>
+
+
+        </Toolbar>
+      </Container>
+  );
+};
 export default Header;
