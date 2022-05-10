14165c31157682b6d2b9a5bfe9442e6ebf5a90d0
Your Name <tariqfaqira@gmail.com>
2022-05-10 09:28:50 +0300

    home header is updated

--- src/layouts/components/HomeHeader.jsx
+++ src/layouts/components/HomeHeader.jsx
@@ -36,7 +36,7 @@ const HomeHeader = () => {
     zIndex: -1,
     width: '75%',
     [theme.breakpoints.up('xl')]: {
-      width: '80%',
+      width: '85%',
       height: '150%'
     },
   }))
@@ -45,10 +45,10 @@ const HomeHeader = () => {
     <Grid container sx={{ height: "576px", position: "relative" }}>
       <Grid item sm={12} md={8} sx={{ position: "relative", m: "10%", display: "flex", alignItems: "center" }}>
         <TextBox>
-          <Typography fontSize={20} sx={{  letterSpacing: "01px" }}>WELCOME TO</Typography>
-          <Typography fontSize={40} color="#252525" fontFamily="muli-bold">LIMELIFE</Typography>
-          <Typography fontSize={40} color="#252525" fontFamily="muli-bold" lineHeight="35px">FIRST NFT</Typography>
-          <Typography fontSize={27} color="#252525" sx={{ letterSpacing: "01px" }}  >STUDIO IN KSA</Typography>
+          <Typography fontSize={23} sx={{  letterSpacing: "01px" }}>WELCOME TO</Typography>
+          <Typography fontSize={43} color="#252525" fontFamily="muli-bold">LIMELIFE</Typography>
+          <Typography fontSize={43} color="#252525" fontFamily="muli-bold" lineHeight="35px">FIRST NFT</Typography>
+          <Typography fontSize={30} color="#252525" sx={{ letterSpacing: "01px" }}  >STUDIO IN KSA</Typography>
         </TextBox>
         <LineBox/>
         {/* <Box sx={{ height: "100%", border: '15px solid #afeb34', width: "75%", position: 'absolute', right: '0px', zIndex: -1 }}>
